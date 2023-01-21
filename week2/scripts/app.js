ef3e738ede4150f4c79cc5621bbcd7f21b70eac6
Jashakan Shanmugathasan <jashakan_2000@hotmail.com>
2023-01-20 23:36:34 -0500

    Week 2 Commit

--- week2/scripts/app.js
+++ week2/scripts/app.js
@@ -1,12 +1,64 @@
-"use strict";
-
-//IIFE - Immediately Invoked Expression
-//AKA - Anonymous Self-Executing Function
-
-(function(){
-    function Start ()
-    {
-        console.log("Application Started!!")
+// IIFE -- Immediatley Invoiding Function Expression
+// AKA -- Anonymous Self-Executing Function
+(function ()
+{
+    function DisplayHomePage()
+    {   
+        console.log("DisplayHomePage");
+        let AboutUsButton = document.getElementById("AboutUsButton")
+        AboutUsButton.addEventListener("click", function ()
+        {
+            location.href = "about.html"
+        });
     }
-    window.addEventListener("load", Start)
+
+    function DisplayProductsPage()
+    {
+        console.log("DisplayProductsPage");
+    }
+
+    function DisplayServicesPage()
+    {
+        console.log("DisplayServicesPage");
+    }
+
+    function DisplayAboutPage()
+    {
+        console.log("DisplayAboutPage");
+    }
+
+    function DisplayContactPage()
+    {
+        console.log("DisplayContactPage");
+    }
+
+    // namesd  function option
+    function Start()
+    {
+        console.log("App Started!!");
+
+        
+        switch (document.title)
+        {
+            case "Home":
+                DisplayHomePage();
+                break;
+            case "Our Products":
+                DisplayProductsPage();
+                break;
+            case "Our Services":
+                DisplayServicesPage();
+                break;
+            case "About Us":
+                DisplayAboutPage();
+                break;
+            case "Contact Us":
+                DisplayContactPage();
+                break;
+        }
+    }
+
+    window.addEventListener("load", Start);
+
+
 })();
