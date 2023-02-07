d0522624cbab4f8a4778bac3be964fbb1686293b
Jaashee <jashakan2000@gmail.com>
2023-02-07 18:06:58 -0500

    Week 4-ICE1 First Commit with edit page and jquery in app.js

---
+++ week4-ICE2/scripts/app.js
@@ -0,0 +1,113 @@
+// IIFE -- Immediately Invoking Function Expression
+// AKA -- Anonymous Self-Executing Function
+(function ()
+{
+    function DisplayHomePage() {
+
+        console.log("DisplayHomePage");
+
+        $("#AboutUsButton").on("click", () => {
+            location.href = "about.html"
+        });
+
+        $("main").append(`<p id="MainParagraph" class="mt-3" >This is the main paragraph<p/>`);
+        $("body").append(`<article class="container">
+                                <p id="ArticleParagraph" class="mt-3">This is my article paragraph</p>\`<article/>`);
+
+    }
+    function DisplayProductsPage()
+    {
+        console.log("Products Page Called!");
+    }
+
+    function DisplayServicesPage()
+    {
+        console.log("Services Page Called!");
+    }
+
+    function DisplayAboutPage()
+    {
+        console.log("About Page Called!");
+    }
+
+    function DisplayContactPage()
+    {
+        console.log("Contact Page Called!");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribedCheckbox = document.getElementById("subscribeCheckBox")
+
+        sendButton.addEventListener("click", function (event )
+        {
+            if (subscribedCheckbox.checked){
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if (contact.serialize()){
+                    let key = contact.FullName.substring(0,1) + Date.now();
+                    localStorage.setItem(key, contact.serialize())
+                }
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        console.log("Contact List Page Called!");
+
+        if(localStorage.length > 0){
+            let contactList = document.getElementById("contactList");
+            let data = "";      // add deserialized data from local storage
+
+            let keys = Object.keys(localStorage);
+
+            let index = 1;
+            for(const key of keys){
+                let contactData = localStorage.getItem(key);
+                let contact = new Contact();
+                contact.deserialize(contactData);
+                data += `<tr><th scope="row" class="text-center">${index}</th>
+                         <td>${contact.FullName}</td>
+                         <td>${contact.ContactNumber}</td>
+                         <td>${contact.EmailAddress}</td>
+                         <td></td>
+                         <td></td>
+                         </tr>`;
+                index++;
+            }
+            contactList.innerHTML = data;
+        }
+    }
+
+    // named  function option
+    function Start()
+    {
+        console.log("App Started!!");
+
+        
+        switch (document.title)
+        {
+            case "Home":
+                DisplayHomePage();
+                break;
+            case "Our Products":
+                DisplayProductsPage();
+                break;
+            case "Our Services":
+                DisplayServicesPage();
+                break;
+            case "About Us":
+                DisplayAboutPage();
+                break;
+            case "Contact Us":
+                DisplayContactPage();
+                break;
+
+            case "Contact List":
+                DisplayContactListPage();
+                break;
+        }
+    }
+
+    window.addEventListener("load", Start);
+
+
+})();
