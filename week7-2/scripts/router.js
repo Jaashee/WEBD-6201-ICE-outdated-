d66aa76f71106ea1d311e5c5c2be812760daa72f
Jaashee <jashakan2000@gmail.com>
2023-03-10 16:08:17 -0500

    Week 7-2 Initial commit with LoadFooter and refactored LoadHeader

---
+++ week7-2/scripts/router.js
@@ -0,0 +1,79 @@
+"use strict";
+
+(function (core) {
+
+    class Router {
+
+        //public properties
+        /**
+         * @returns {string}
+         */
+        get AvtiveLink() {
+            return this.m_activeLink;
+        }
+
+        /**
+         * @param string
+         */
+        set ActiveLink(link) {
+            this.m_activeLink = link;
+        }
+
+        //Constructor
+        constructor() {
+            this.ActiveLink = "";
+        }
+
+        //Public methods
+        Add(route) {
+            this.m_routingTable.push(route);
+        }
+
+        AddTable(routeTable) {
+            this.m_routingTable = routeTable;
+        }
+
+        Find(route) {
+            this.m_routingTable.indexOf(route);
+        }
+
+        Remove(route) {
+            if(this.Find(route) > -1){
+                this.m_routingTable.splice(this.Find(route), 1)
+                return true;
+            }
+            return false;
+        }
+
+
+        //Public override methods
+        toString() {
+            return this.m_routingTable.toString();
+        }
+
+    }
+
+})(core || (core = {}));
+
+let router = new core.Router();
+
+router.AddTable(
+    [
+        "/",
+        "/home",
+        "/about",
+        "/services",
+        "/contact",
+        "/contact-list",
+        "/products",
+        "/register",
+        "/login",
+        "/edit",
+    ]
+);
+
+let route = location.pathname;
+
+router.ActiveLink = (router.Find(route) > -1) // IF true(?) do what's next
+                    ? (route === "/") ? "home" : route.substring(1) // if false do what's next
+                    : ("404");
