1e23f2acd0ce445df9431aed5dc8a1ecbced8b12
Jaashee <jashakan2000@gmail.com>
2023-03-10 20:24:07 -0500

    Week 9 commit refactored authguard.js, namespace.js, router.js to typescript file (.ts)

--- week9/scripts/authguard.ts
+++ week9/scripts/authguard.ts
@@ -1,10 +1,14 @@
 "use strict";
+
 (function () {
-    let protected_routes = ["contact-list.html"];
+
+    let protected_routes: string[] = ["contact-list.html"]
+
     if (protected_routes.indexOf(router.ActiveLink) > -1) {
         if (!sessionStorage.getItem("user")) {
             location.href = "/login";
         }
     }
+
+
 })();
-//# sourceMappingURL=authguard.js.map
--- week9/scripts/router.js
+++ week9/scripts/router.js
@@ -1,81 +1,56 @@
 "use strict";
-
+var core;
 (function (core) {
-
     class Router {
-
-        //public properties
-        /**
-         * @returns {string}
-         */
+        m_activeLink;
+        m_routingTable;
         get ActiveLink() {
             return this.m_activeLink;
         }
-
-        /**
-         * @param string
-         */
         set ActiveLink(link) {
             this.m_activeLink = link;
         }
-
-        //Constructor
         constructor() {
-            this.ActiveLink = "";
+            this.m_activeLink = "";
+            this.m_routingTable = [];
         }
-
-        //Public methods
         Add(route) {
             this.m_routingTable.push(route);
         }
-
         AddTable(routeTable) {
             this.m_routingTable = routeTable;
         }
-
         Find(route) {
-           return this.m_routingTable.indexOf(route);
+            return this.m_routingTable.indexOf(route);
         }
-
         Remove(route) {
-            if(this.Find(route) > -1){
-                this.m_routingTable.splice(this.Find(route), 1)
+            if (this.Find(route) > -1) {
+                this.m_routingTable.splice(this.Find(route), 1);
                 return true;
             }
             return false;
         }
-
-
-        //Public override methods
         toString() {
             return this.m_routingTable.toString();
         }
-
     }
-
     core.Router = Router;
-
 })(core || (core = {}));
-
 let router = new core.Router();
-
-router.AddTable(
-    [
-        "/",
-        "/home",
-        "/about",
-        "/services",
-        "/contact",
-        "/contact-list",
-        "/products",
-        "/register",
-        "/login",
-        "/edit",
-    ]
-);
-
+router.AddTable([
+    "/",
+    "/home",
+    "/about",
+    "/services",
+    "/contact",
+    "/contact-list",
+    "/products",
+    "/register",
+    "/login",
+    "/edit",
+]);
 let route = location.pathname;
-
-router.ActiveLink = (router.Find(route) > -1) // IF true(?) do what's next
-                    ? (route == "/") ? "home" : route.substring(1) // if false do what's next
-                    : "404";
+router.ActiveLink = (router.Find(route) > -1)
+    ? (route == "/") ? "home" : route.substring(1)
+    : "404";
+//# sourceMappingURL=router.js.map
