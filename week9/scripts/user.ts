f6a4fc13a750034e0bab251b9b9d648fd57fffb0
Jaashee <jashakan2000@gmail.com>
2023-03-10 20:36:41 -0500

    Week 9 commit refactored user.js to user.ts

--- week9/scripts/user.ts
+++ week9/scripts/user.ts
@@ -1,67 +1,85 @@
 "user strict";
+
 "use strict";
-var core;
-(function (core) {
-    class User {
-        m_displayName;
-        m_emailAddress;
-        m_username;
-        m_password;
-        constructor(displayName = "", emailAddress = "", username = "", password = "") {
+
+namespace core {
+
+    export class User {
+        private m_displayName: string;
+        private m_emailAddress: string;
+        private m_username: string;
+        private m_password: string;
+
+        constructor(displayName: string = "", emailAddress: string = "", username: string = "", password: string = "") {
             this.m_displayName = displayName;
             this.m_emailAddress = emailAddress;
             this.m_username = username;
             this.m_password = password;
         }
-        get DisplayName() {
+
+        // Getters and Setters
+        public get DisplayName(): string {
             return this.m_displayName;
         }
-        set DisplayName(displayName) {
+
+        public set DisplayName(displayName: string) {
             this.m_displayName = displayName;
         }
-        get EmailAddress() {
+
+        public get EmailAddress(): string {
             return this.m_emailAddress;
         }
-        set EmailAddress(emailAddress) {
+
+        public set EmailAddress(emailAddress: string) {
             this.m_emailAddress = emailAddress;
         }
-        get Username() {
+
+        public get Username(): string {
             return this.m_username;
         }
-        set Username(username) {
+
+        public set Username(username: string) {
             this.m_username = username;
         }
-        get Password() {
+
+        public get Password(): string {
             return this.m_password;
         }
-        set Password(password) {
+
+        public set Password(password: string) {
             this.m_password = password;
         }
-        toString() {
+
+        //overridden methods
+        public toString(): string {
             return `Display Name: ${this.DisplayName}\n
                     Email Address: ${this.EmailAddress}\nUsername: ${this.Username}`;
         }
-        toJSON() {
+
+        public toJSON(): { Username: string, DisplayName: string, EmailAddress: string} {
             return {
                 "DisplayName": this.m_displayName,
                 "EmailAddress": this.m_emailAddress,
                 "Username": this.m_username
-            };
+            }
         }
-        fromJSON(data) {
+
+        public fromJSON(data : User) {
             this.m_displayName = data.DisplayName;
             this.m_emailAddress = data.EmailAddress;
             this.m_username = data.Username;
             this.m_password = data.Password;
         }
-        serialize() {
+
+        public serialize() : string | null {
             if (this.DisplayName !== "" && this.EmailAddress !== "" && this.Username !== "" && this.Password !== "") {
                 return `${this.DisplayName}, ${this.EmailAddress}, ${this.Username}, ${this.Password} `;
             }
             console.error("One or more the properties of the Contact object are missing or invalid");
             return null;
         }
-        deserialize(data) {
+
+        public deserialize(data : string) {
             let propertyArray = data.split(",");
             this.m_displayName = propertyArray[0];
             this.m_emailAddress = propertyArray[1];
@@ -69,6 +87,4 @@ var core;
             this.m_password = propertyArray[3];
         }
     }
-    core.User = User;
-})(core || (core = {}));
-//# sourceMappingURL=user.js.map
+}
